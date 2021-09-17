6745aa17154b90af003dcd02471eeffebde951e4
maiphuong30 <maiphuong30.12.2000@gmail.com>
2021-09-17 18:55:28 +0700

    first commit

---
+++ caro.js
@@ -0,0 +1,17 @@
+function Loaded()
+{
+	var size = document.getElementById("size").value;
+	var table = document.getElementById("board");
+	var	row = document.getElementsByClassName("boardRow");
+	var square = document.getElementsByClassName("square");
+	// Vẽ board game
+	table.innerHTML = "";
+	for (y = 0; y < size; y++){
+		table.innerHTML += '<div class="boardRow" style="height:'+100/size+'%"></div>';
+		for (x = 0; x < size; x++){
+			var div = '<div class="square"></div>';
+			row.item(y).innerHTML += '<div class="boardCol" style="width:'+100/size+'%">'+div+'</div>';
+			square.item(x+y*size).setAttribute("id",(x+y*size).toString());
+		}
+	}
+}
