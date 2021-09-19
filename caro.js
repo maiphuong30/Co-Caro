590c478d57e6bd2d75fea3766110266698cbd90b
maiphuong30 <maiphuong30.12.2000@gmail.com>
2021-09-19 10:06:20 +0700

    current player

--- caro.js
+++ caro.js
@@ -9,7 +9,7 @@ function Loaded()
 	for (y = 0; y < size; y++){
 		table.innerHTML += '<div class="boardRow" style="height:'+100/size+'%"></div>';
 		for (x = 0; x < size; x++){
-			var div = '<div class="square" onClick="Click(id)"></div>';
+			var div = '<div class="square" onClick="Click(id)" onMouseOver="MouseOver(id)" onMouseOut="MouseOut(id)"></div>';
 			row.item(y).innerHTML += '<div class="boardCol" style="width:'+100/size+'%">'+div+'</div>';
 			square.item(x+y*size).setAttribute("id",(x+y*size).toString());
 			square.item(x+y*size).setAttribute("player","-1");
--- click.js
+++ click.js
@@ -9,6 +9,23 @@ function Click(id)
 	if (square.item(pos).getAttribute("player") != "-1") return;
 	square.item(pos).style.backgroundImage = path;
 	square.item(pos).setAttribute("player",currValue.toString());
+	//Đổi lượt
 	if (currValue == 0) currValue = 1;
 	else currValue = 0;
+	var iplayer = "url('Images/X.png')";
+	if (currValue == 1) iplayer = "url('Images/O.png')";
+	var imgp = document.getElementById("imgPlayer");
+	imgp.style.backgroundImage = iplayer;
+}
+function MouseOver(id)
+{
+	var square = document.getElementsByClassName("square");
+	var pos = parseInt(id);
+	square.item(pos).style.backgroundColor = "#2ecc71";
+}
+function MouseOut(id)
+{
+	var square = document.getElementsByClassName("square");
+	var pos = parseInt(id);
+	square.item(pos).style.backgroundColor = "#34495e";
 }
